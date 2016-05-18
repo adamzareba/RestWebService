d467ad2ef9367b31114d5eabb20c5f1b65507a7c
bontade88 <bontade88@gmail.com>
2016-05-18 22:08:47 +0200

    Initial commit

---
+++ src/main/java/com/bontade/web/service/rest/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.bontade.web.service.rest.controller;
+
+import com.bontade.web.service.rest.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by zarebaa on 18/05/2016.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
